85760e8f98a9d9efc6f34073d6dd5c6849a14186
@hexlet/cli <support@hexlet.io>
2023-01-26 17:20:59 +0600

    submit java-oop-ru/patterns

--- java-oop-ru/patterns/src/main/java/exercise/TcpConnection.java
+++ java-oop-ru/patterns/src/main/java/exercise/TcpConnection.java
@@ -1,38 +1,38 @@
 package exercise;
 
 // BEGIN
-public class TcpConnection{
+public class TcpConnection {
 
 	private String host;
 	private int port;
 	private Connection state = new Disconnected();
 
-	public TcpConnection(String host, int port){
+	public TcpConnection(String host, int port) {
 		this.host = host;
 		this.port = port;
 	}
 
-	public String getCurrentState(){
+	public String getCurrentState() {
 		return state.getCurrentState();
 	}
 
-	public void connect(){
+	public void connect() {
 		state.connect(this);
 	}
 
-	public void disconnect(){
+	public void disconnect() {
 		state.disconnect(this);
 	}
 
-	public void write(String message){
+	public void write(String message) {
 		state.write(message);
 	}
 
-	public void setState(Connection connection){
+	public void setState(Connection connection) {
 		this.state = connection;
 	}
 
-	public Connection getState(){
+	public Connection getState() {
 		return state;
 	}
 
